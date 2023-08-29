74b3983f7f05236157ca6aa0584a454c6883de94
kumaresan2001 <kumaresancrseven@gmail.com>
2023-08-29 12:58:00 +0530

    new in db

--- service/tournament.service.js
+++ service/tournament.service.js
@@ -2,13 +2,13 @@ import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function posttournament(data) {
-  return await client.db("indiggg").collection("tournament").insertOne(data);
+  return await client.db("tourn").collection("tournaments").insertOne(data);
 }
 
 export async function alltournament(query) {
   return await client
-    .db("indiggg")
-    .collection("tournament")
+    .db("tourn")
+    .collection("tournaments")
     .find(query)
     .toArray();
 }
@@ -16,21 +16,21 @@ export async function alltournament(query) {
 export async function idbytournment(id) {
   console.log(typeof id, id);
   return await client
-    .db("indiggg")
-    .collection("tournament")
+    .db("tourn")
+    .collection("tournaments")
     .findOne({ _id: new ObjectId(id) });
 }
 
 export async function updatetournament(id, data) {
   return await client
-    .db("indiggg")
-    .collection("tournament")
+    .db("tourn")
+    .collection("tournaments")
     .updateOne({ _id: new ObjectId(id) }, { $set: data });
 }
 
 export async function deletetournament(id) {
   return await client
-    .db("indiggg")
-    .collection("tournament")
+    .db("tourn")
+    .collection("tournaments")
     .deleteOne({ _id: new ObjectId(id) });
 }
