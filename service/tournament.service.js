671db1ab5392a2d3769fc6459757b8df842c931b
kumaresan2001 <kumaresancrseven@gmail.com>
2023-08-29 12:41:27 +0530

    new change

--- service/tournament.service.js
+++ service/tournament.service.js
@@ -2,12 +2,12 @@ import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function posttournament(data) {
-  return await client.db("indigg").collection("tournament").insertOne(data);
+  return await client.db("indiggg").collection("tournament").insertOne(data);
 }
 
 export async function alltournament(query) {
   return await client
-    .db("indigg")
+    .db("indiggg")
     .collection("tournament")
     .find(query)
     .toArray();
@@ -16,21 +16,21 @@ export async function alltournament(query) {
 export async function idbytournment(id) {
   console.log(typeof id, id);
   return await client
-    .db("indigg")
+    .db("indiggg")
     .collection("tournament")
     .findOne({ _id: new ObjectId(id) });
 }
 
 export async function updatetournament(id, data) {
   return await client
-    .db("indigg")
+    .db("indiggg")
     .collection("tournament")
     .updateOne({ _id: new ObjectId(id) }, { $set: data });
 }
 
 export async function deletetournament(id) {
   return await client
-    .db("indigg")
+    .db("indiggg")
     .collection("tournament")
     .deleteOne({ _id: new ObjectId(id) });
 }
